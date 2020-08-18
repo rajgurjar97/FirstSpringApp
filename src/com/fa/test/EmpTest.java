3fb8bead092b8a4027c7df8b5d0a459702a40d4b
rajgurjar97 <rajgurjarji97@gmail.com>
2020-08-18 11:05:22 +0530

    added git-demo project to local repo

---
+++ src/com/fa/test/EmpTest.java
@@ -0,0 +1,17 @@
+package com.fa.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+
+
+public class EmpTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi Rajesh Gurjar");
+		
+	}
+
+}
